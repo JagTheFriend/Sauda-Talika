2873867dd6b3f850d22aa5db2b0ea6a746292e7a
JagTheFriend <JagTheFriend12@gmail.com>
2025-07-10 10:58:48 +0530

    Added shortcuts
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -21,10 +21,30 @@ export default defineConfig(({ mode }) => ({
         short_name: "Sauda Talika",
         description:
           "Sauda Talika - Making grocery shopping and meal planning effortless through intelligent list management and AI-powered recipe suggestions.",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
+        theme_color: "#000000",
+        background_color: "#000000",
         display: "standalone",
         start_url: "/",
+        shortcuts: [
+          {
+            name: "Add List",
+            short_name: "Add List",
+            description: "Add a new list",
+            url: "/dashboard",
+          },
+          {
+            name: "Search Recipes",
+            short_name: "Search Recipes",
+            description: "Search for recipes using AI",
+            url: "/recipes",
+          },
+          {
+            name: "About Us",
+            short_name: "About Us",
+            description: "About Sauda Talika",
+            url: "/about",
+          },
+        ],
         icons: [
           {
             src: "/app-icon/pwa-192x192.png",
